14b6c55713885e3425c3727118a5b6d9a7652190
Sayura Silva <your-email@example.com>
2025-07-18 00:17:03 +0530

    Finished the ThemeToggle.jsx and imported it to Home.jsx

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import { ThemeToggle } from "../components/ThemeToggle"
 
 
 export const Home = () => {
-    return <div>Home</div>
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">  
+
+        {/* Theme Toggle */}
+        <ThemeToggle />
+
+        {/* Background Effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+    </div>
 }
